779bd878ca7cd7b3b416773fcadbbc9d984aeb7b
Maple <120883257+Wanfeng114@users.noreply.github.com>
2024-10-26 21:30:32 +0800

    更新 Noslow.java
    起床跑吃跑喝完整版

--- modules/movement/Noslow.java
+++ modules/movement/Noslow.java
@@ -1,14 +1,15 @@
 恭喜你啊 兄弟 被欣欣哥恭喜到了 现在你可以在起床跑吃跑喝了
-if (packet instanceof CPacketPlayerTryUseItem) {
-                    setStrafeAndForward(0.2f);
-                    event.setCancelled(true);
-                    PacketUtils.sendPacketNoEvent(packet);
-                    mc.getConnection().sendPacket(new CPacketChatMessage("/lizi open"));
-                }
-                if (packet instanceof SPacketOpenWindow) {
-                    if (((SPacketOpenWindow) packet).getWindowTitle().getUnformattedText().contains("特效")) {
-                        event.setCancelled(true);
-                        setStrafeAndForward(1f);
-                        mc.getConnection().sendPacket(new CPacketCloseWindow(((SPacketOpenWindow) packet).getWindowId()));
+我觉得你这个Shovel抄袭了SilenceFix 因为Shovel在八月就有了而SilenceFix在11月才有的 所以肯定是抄袭了啊，不然怎么可能时间会更早呢？
+if (packet is C08PacketPlayerBlockPlacement && packet.position.y == -1) {
+                    if ((mc.thePlayer.heldItem.item is ItemFood && mc.thePlayer.heldItem.item !is ItemAppleGold && mc.thePlayer.foodStats.foodLevel < 20.0F) || mc.thePlayer!!.heldItem.item is ItemAppleGold || (mc.thePlayer!!.heldItem.item is ItemPotion && !ItemPotion.isSplash(mc.thePlayer.heldItem.itemDamage))) {
+                        shouldSlow = true
+                        sendPacketNoEvent(C01PacketChatMessage("/lizi open"))
+                        sendChat = true
                     }
+                }
+                if (packet is S2DPacketOpenWindow && sendChat) {
+                    event.cancelEvent()
+                    shouldSlow = false
+                    sendChat = false
+                    sendPacketNoEvent(C0DPacketCloseWindow(packet.windowId))
                 }
