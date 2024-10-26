b0356fe0b38e27751bb89f36287af85ef3365a8b
Maple <120883257+Wanfeng114@users.noreply.github.com>
2024-10-26 20:59:26 +0800

    更新 Noslow.java
    起床跑吃跑喝

--- modules/movement/Noslow.java
+++ modules/movement/Noslow.java
@@ -1,9 +1,14 @@
-还想在起床吃东西，滚。
-半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07
-        半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07
-半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07
-        半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07
-半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07
-        半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07
-半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07
-        半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07半个C07
+恭喜你啊 兄弟 被欣欣哥恭喜到了 现在你可以在起床跑吃跑喝了
+if (packet instanceof CPacketPlayerTryUseItem) {
+                    setStrafeAndForward(0.2f);
+                    event.setCancelled(true);
+                    PacketUtils.sendPacketNoEvent(packet);
+                    mc.getConnection().sendPacket(new CPacketChatMessage("/lizi open"));
+                }
+                if (packet instanceof SPacketOpenWindow) {
+                    if (((SPacketOpenWindow) packet).getWindowTitle().getUnformattedText().contains("特效")) {
+                        event.setCancelled(true);
+                        setStrafeAndForward(1f);
+                        mc.getConnection().sendPacket(new CPacketCloseWindow(((SPacketOpenWindow) packet).getWindowId()));
+                    }
+                }
