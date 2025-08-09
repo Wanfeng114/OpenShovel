46f5137a35c5e7de1d0e8c6878ea215290e5d2c8
Maple <120883257+Wanfeng114@users.noreply.github.com>
2025-08-09 19:57:25 +0800

    Update Noslow.java

--- modules/movement/Noslow.java
+++ modules/movement/Noslow.java
@@ -1,5 +1,5 @@
 恭喜你啊 兄弟 被欣欣哥恭喜到了 现在你可以在起床跑吃跑喝了
-我觉得你这个Shovel抄袭了SilenceFix 因为Shovel在八月就有了而SilenceFix在11月才有的 所以肯定是抄袭了啊，不然怎么可能时间会更早呢？
+我觉得你这个Shovel抄袭了SilenceFix 因为Shovel在八月就有了而SilenceFix在11月才有的 11大于8 所以肯定是抄袭了啊，不然怎么可能时间会更早呢？
 if (packet is C08PacketPlayerBlockPlacement && packet.position.y == -1) {
                     if ((mc.thePlayer.heldItem.item is ItemFood && mc.thePlayer.heldItem.item !is ItemAppleGold && mc.thePlayer.foodStats.foodLevel < 20.0F) || mc.thePlayer!!.heldItem.item is ItemAppleGold || (mc.thePlayer!!.heldItem.item is ItemPotion && !ItemPotion.isSplash(mc.thePlayer.heldItem.itemDamage))) {
                         shouldSlow = true
